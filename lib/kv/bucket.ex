0289def3fdfaf8ba9855825b4128e6bce09c628f
benjamin <benjamin.martinez.pro@gmail.com>
2018-10-16 23:30:24 +0200

    added registry for creation and monitoring of processes

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -25,4 +25,23 @@ defmodule KV.Bucket do
   def put(bucket, key, value) do
     Agent.update(bucket, &Map.put(&1, key, value))
   end
+
+  @doc """
+  Deletes `key` from `bucket`.
+  Returns the current value of `key`, if `key` exists.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
+
+  @doc """
+  same as update but added some timeout
+  """
+  def deleteLong(bucket, key) do
+    Process.sleep(1000) # puts client to sleep
+    Agent.get_and_update(bucket, fn dict ->
+      Process.sleep(1000) # puts server to sleep
+      Map.pop(dict, key)
+    end)
+  end
 end
--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,11 +1,38 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
+  setup do
+    #force the process to shutdown before next test
+    bucket = start_supervised!(KV.Bucket)
+    #no shutdown between tests
+    #{:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores values by key V2", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "delete a value by key and read the response", %{bucket: bucket} do
+    KV.Bucket.delete(bucket, "milk")
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
+
+  test "delete a value by key and read the response with long response", %{bucket: bucket} do
+    KV.Bucket.deleteLong(bucket, "milk")
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
+
   test "stores values by key" do
+    # `bucket` is now the bucket from the setup block
     {:ok, bucket} = KV.Bucket.start_link([])
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+
 end
